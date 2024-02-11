96e855ddcb99e75ab01820b16667405f78fca806
soren <afrozaman12@gmail.com>
2024-02-12 04:59:18 +0600

    Fixed OG image URL

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import Preloader from "./components/Preloader";
 import CustomCursor from "./components/CustomCursor";
 
 export const metadata = {
-  metadataBase: "https://personal-brand-portfolio-nextjs.vercel.app",
+  metadataBase: "https://next-personal-brand-portfolio.vercel.app",
   title: "Soren Blank",
   description: "I'm Soren D. Blank & I have a team of professionals who can help you grow your business. We offer web development, digital marketing, and graphics design services. We are professional, creative, and responsive. We are here to help you grow your business. Period.",
   image: "https://next-vcard-portfolio.vercel.app/embed.png",
@@ -12,14 +12,14 @@ export const metadata = {
 
     images: [
       {
-        url: "https://personal-brand-portfolio-nextjs.vercel.app/og-image.png",
+        url: "https://next-personal-brand-portfolio.vercel.app/og-image.png",
       },
     ],
   },
   twitter: {
     images: [
       {
-        url: "https://personal-brand-portfolio-nextjs.vercel.app/og-image.png",
+        url: "https://next-personal-brand-portfolio.vercel.app/og-image.png",
       },
     ]
   },
